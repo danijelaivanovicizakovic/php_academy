b756242868825ddb7bc8c035ac68864db908fb1e
danijelaivanovicizakovic <danijela.ivanovic.izakovic@gmail.com>
2016-10-27 12:14:54 +0200

    Zadatak

---
+++ zadatak_3_vj/form.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <title>Prijavi se</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond|Sanchez" rel="stylesheet">
+</head>
+<body>
+
+<header>
+    <center><img src="php_akademija.jpg"></center>
+    <div id=navigacija>
+    <ul>
+        <li><a href="index.php">Naslovnica</a></li>
+        <li><a href="form.php">Prijavi se</a></li>
+        <li><a href="admin.txt">Login (za admine)</a></li>
+    </ul>
+    </div>
+</header>
+
+<main>
+
+    <h1>Prijavnica za PHP akademiju</h1>
+
+    <p>Prijavnica za prvo osječko izdanje PHP akademije koju Inchoo pokreće u suradnji s FERITom.</p>
+    <p>Prijave traju do 10.10., pa požuri i svoje mjesto rezerviraj već sad.</p>
+    <p>Više informacija na:
+        <a href="http://inchoo.hr/php-akademija-2016/" target="_blank">http://inchoo.hr/php-akademija-2016/</a>
+    </p>
+
+<form action="podaci.php" method="post">
+    <label>Ime i prezime: </label>
+    <input type="text" name="ime"/><br><br>
+
+    <label>e-mail adresa: </label>
+    <input type="email" name="email"/><br><br>
+
+    <label>Smjer: </label>
+    <input type="text" name="smjer"/><br>
+
+    <br><label>Godina studija: </label><br>
+    <input type="radio" name="godina"/><label>1</label><br>
+    <input type="radio" name="godina"/><label>2</label><br>
+    <input type="radio" name="godina"/><label>3</label><br>
+    <input type="radio" name="godina"/><label>4</label><br>
+    <input type="radio" name="godina"/><label>5</label><br>
+
+    <br><label>Što te motiviralo da se prijaviš?</label>
+    <textarea rows="5" cols="50" name="motivacija"> </textarea><br>
+
+    <br><label>Imaš li predznanje vezano uz web development?</label>
+    <textarea rows="5" cols="50" name="predznanje"> </textarea><br>
+
+    <br>U kojim jezicima si do sada programirao?<br>
+    <input type="checkbox" name="jezici"  value="c"/><label>C/C++</label><br>
+    <input type="checkbox" name="jezici"  value="java"/><label>Java</label><br>
+    <input type="checkbox" name="jezici"  value="python"/><label>Python</label><br>
+    <input type="checkbox" name="jezici"  value="R"/><label>R</label><br>
+    <input type="checkbox" name="jezici"  value="c#"/><label>C#</label><br>
+    <input type="checkbox" name="jezici"  value="php"/><label>PHP</label><br>
+    <input type="checkbox" name="jezici"  value="js"/><label>JavaScript</label><br>
+    <input type="checkbox" name="jezici"  value="ruby"/><label>Ruby</label><br>
+
+    <br>Uploadaj primjer svoga koda: <input type="file" name="datoteka"  value="Odaberi datoteku"><br>
+
+    <input type="submit">
+</form>
+
+</main>
+
+<footer>
+    <p>&copy; PHP Akademija, 2016</p>
+</footer>
+
+</body>
+</html>
---
+++ zadatak_3_vj/podaci.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Podaci</title>
+</head>
+<body>
+<header>
+    <center><img src="php_akademija.jpg"></center>
+    <div id=navigacija>
+    <ul>
+        <li><a href="index.php">Naslovnica</a></li>
+        <li><a href="form.php">Prijavi se</a></li>
+        <li><a href="admin.txt">Login (za admine)</a></li>
+    </ul>
+    </div>
+</header>
+<?php
+$ime = $email = $smjer = $godina = $jezici = $motivacija = $predznanje ="";
+$datoteka ="";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+    $ime = test_input($_POST["ime"]);
+    $email = test_input($_POST["email"]);
+    $smjer = test_input($_POST["smjer"]);
+    $godina = test_input($_POST["godina"]);
+    $prijasnjiJezici = test_input($_POST["jezici"]);
+    $motivacija = test_input($_POST["motivacija"]);
+    $predznanje = test_input($_POST["predznanje"]);
+}
+
+function test_input($data) 
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+$prijave = fopen("prijave.txt", "w") or die("Unable to open file!");
+fwrite($prijave, $ime);
+fwrite($prijave, $email);
+fwrite($prijave, $smjer);
+fwrite($prijave, $godina);
+fwrite($prijave, $jezici);
+fwrite($prijave, $motivacija);
+fwrite($prijave, $predznanje);
+?>
+
+<?php
+	echo ("Poštovani ".$ime." zahvaljujemo ti na prijavi"."<br>"); 
+	echo ("Sve obavijsti vezane uz PHP akademiju poslati ćemo ti na ovaj: ".$email . "e-mail"."<br>"); 
+?>
+</body>
+</html>
